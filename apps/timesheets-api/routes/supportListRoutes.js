bc2b7d5cb1189771eb12eb21679167b51e821a90
Tisha Murvihill <tmurv@shaw.ca>
2024-06-22 11:58:40 -0600

    remove deprecated google secret keys

--- apps/timesheets-api/routes/supportListRoutes.js
+++ apps/timesheets-api/routes/supportListRoutes.js
@@ -4,6 +4,5 @@ const supportListController = require('../controllers/supportListController');
 
 router.get('/currentjobs', supportListController.getCurrentJobs);
 router.get('/tasks', supportListController.getTasks);
-router.get('/lunchtimes', supportListController.getLunchTimes);
 
 module.exports = router;
